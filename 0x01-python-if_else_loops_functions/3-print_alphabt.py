1814f26d816cf4fbd3c94837316bc22e5a1eca3d
JamesCicada <mabdoox@gmail.com>
2023-10-31 15:26:41 -0700

    Mandatory Tasks

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 for letter in range(ord('a'), ord('z') + 1):
-    if letter != 'q' and letter != 'e':
+    if letter not in 'qe':
         print("{}".format(chr(letter)), end='')
