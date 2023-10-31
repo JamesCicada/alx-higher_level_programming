7554a8bc1b56457af9d954ed74160cd79bb54e5d
JamesCicada <mabdoox@gmail.com>
2023-10-31 15:30:47 -0700

    Mandatory Tasks

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 for letter in range(ord('a'), ord('z') + 1):
-    valid = letter == 'e' or letter == 'q'
-        if valid: == False:
+    if letter != ord('e') and letter != ord('q'):
         print("{}".format(chr(letter)), end='')
