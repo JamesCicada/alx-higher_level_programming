ae97f379570159b3114f3b1bb5b3975d763d769e
JamesCicada <mabdoox@gmail.com>
2023-10-31 18:38:33 -0700

    Mandatory Tasks

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def uppercase(str):
+    output = ""
     for char in str:
         if ord('a') <= ord(char) <= ord('z'):
             char = chr(ord(char) - 32)
-        print(char, end="")
+        output += char
+    print("{}".format(output))
