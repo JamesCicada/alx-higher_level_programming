229dbd93293995a67e01ca2916672d1f1de75f1b
JamesCicada <mabdoox@gmail.com>
2023-11-06 18:22:20 -0800

    Mandatory Tasks

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
 
     a = 10
     b = 5
-
-    print("{} + {} = {}".format(a, b, add(a, b))
-          "{} - {} = {}".format(a, b, sub(a, b))
-          "{} * {} = {}".format(a, b, mul(a, b))
-          "{} / {} = {}".format(a, b, div(a, b)))
+    
+    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{a} - {b} = {sub(a, b)}")
+    print(f"{a} * {b} = {mul(a, b)}")
+    print(f"{a} / {b} = {div(a, b)}")
