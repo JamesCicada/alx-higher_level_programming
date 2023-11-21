6a12b32d173e8177d67cd49658bc3a8171ea0dc2
JamesCicada <mabdoox@gmail.com>
2023-11-21 03:06:02 -0800

    Mandatory Tasks

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,4 +3,9 @@
 """Define a Square Class"""
 class Square:
     """Represents a square."""
-    __size = 10
+    def __init__(self, size):
+        """Init a new Square.
+
+        Args:
+            size (int): size of the new square."""
+        self.__size = size
