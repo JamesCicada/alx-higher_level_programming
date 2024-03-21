cc53f047e2cd06fce6f639afd4eef88825736949
JamesCicada <mabdoox@gmail.com>
2024-03-20 19:03:18 -0700

    Mandatory Tasks

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,11 +2,13 @@
 
 def best_score(a_dictionary):
     best_score = 0
+    name = ""
     if bool(a_dictionary):
         for key in a_dictionary:
             score = a_dictionary.get(key)
             if score > best_score:
                 best_score = score
-        return best_score
+                name = key
+        return name
     else:
         return None
