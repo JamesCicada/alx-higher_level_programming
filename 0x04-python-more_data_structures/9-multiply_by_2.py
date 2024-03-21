ba1dfc673be6a8dfb2b5e034689f698b7d38057f
JamesCicada <mabdoox@gmail.com>
2024-03-20 18:50:43 -0700

    Mandatory Tasks

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,5 +3,5 @@
 def multiply_by_2(a_dictionary):
     result = {}
     for k in a_dictionary:
-        result.update({key: a_dictionary.get(key) * 2})
+        result.update({k: a_dictionary.get(k) * 2})
     return result
