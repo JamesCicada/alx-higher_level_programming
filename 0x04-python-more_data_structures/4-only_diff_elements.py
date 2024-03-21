008d18561758cb880ee448b1e934cdc7d1bec22d
JamesCicada <mabdoox@gmail.com>
2024-03-20 18:19:17 -0700

    Mandatory Tasks

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -3,7 +3,9 @@
 def only_diff_elements(set_1, set_2):
     diff = []
     for i in set_1:
-        for j in set_2:
-            if i != j:
-                diff.append(j)
+        if i not in diff and i not in set_2:
+            diff.append(i)
+    for j in set_2:
+        if j not in diff and j not in set_1:
+            diff.append(j)
     return diff
