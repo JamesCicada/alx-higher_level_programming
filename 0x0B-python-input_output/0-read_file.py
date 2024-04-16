5bd20e8a167bf8ac082c7bb6ec20bea7adeaaf8f
JamesCicada <mabdoox@gmail.com>
2024-04-16 09:49:32 -0700

    Mandatory Tasks

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""Defines a file-reading function."""
 
-'''Reads and Prints the content of a file''' 
+
 def read_file(filename=""):
+    """Print content of a UTF8 text file"""
     with open(filename, encoding="utf-8") as f:
-        print(f.read(), end='')
+        print(f.read(), end="")
