2235cb851326353c92458a58ad43d765c02d1387
JamesCicada <mabdoox@gmail.com>
2024-04-16 10:01:08 -0700

    Mandatory Tasks

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,6 +2,14 @@
 """Defines a function that writes a string to a file."""
 
 def write_file(filename="", text=""):
-    """Writes the string to the file."""
+    """Writes a string to a UTF-8 text file.
+
+    Args:
+    filename (str): The name of the file to write.
+    text (str): The text to write to the file.
+
+    Returns:
+    int: The number of characters written.
+    """
     with open(filename, encoding="utf-8") as f:
-        f.write(text)
+        return f.write(text)
