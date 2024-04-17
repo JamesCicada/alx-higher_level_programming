d1caa804afaa35ff95dc22675e0f6fc7803a92eb
JamesCicada <mabdoox@gmail.com>
2024-04-17 10:33:43 -0700

    Mandatory Tasks

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,6 +2,6 @@
 """This script defines a function for retrieving object attributes."""
 
 
-def get_attributes(obj):
+def lookup(obj):
     """Retrieve and return a list of available attributes for the given object."""
     return dir(obj)
